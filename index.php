eba203a33ece0f28b864bb4dc9419a184dbe30d6
Niklas Hösl <niklas.hoesl@runtastic.com>
2019-03-05 07:59:19 +0100

    MOD time format

--- index.php
+++ index.php
@@ -2,23 +2,24 @@
 </textarea>
 <button onclick="toHumanReadableDate()" style="width: 100%; height: 40px; font-size: 15pt;">Let me understand those millis!</button>
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.js"></script>
 <script>
 function toHumanReadableDate(){
   var value = document.getElementById("input").value
 
-  var expression = / ([0-9]{13})/g;
+  var expression = /[^0-9](1[0-9]{12})[^0-9]/g;
   var s = value
   var m;
   var res = []
 
   do {
       m = expression.exec(value);
-      if (m) res.push(m[0])
+      if (m) res.push(m[1])
   } while (m);
 
   res.forEach(v=>{
-    dateStr = new Date(parseInt(v));
-    value = value.replace(v," "+dateStr)
+    dateStr = moment(new Date(parseInt(v))).format("YYYY-MM-DD HH:mm");   
+    value = value.replace(v,`\"${dateStr} UTC\"`)
   });
 
   document.getElementById("input").value = value;
